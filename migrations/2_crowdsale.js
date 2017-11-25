8e3ccb91a06cc47a9c9a00f3b012bedd3e54c296
m_maglovany <maksym.maglovanyi@yandex.ru>
2017-11-25 12:07:50 +0200

    crowdsale fix

--- migrations/2_crowdsale.js
+++ migrations/2_crowdsale.js
@@ -1,20 +1,25 @@
-var testData = require("./config.json")[0]
+var testData = require("./config.json")[0];
  
 var EarlyContribStrategy = artifacts.require("./EarlyContribStrategy.sol");
 var PreIcoStrategy = artifacts.require("./PreIcoStrategy.sol");
 var TrancheStrategy = artifacts.require("./TrancheStrategy.sol");
 var FinalizableMowjow = artifacts.require("./FinalizableMowjow.sol"); 
 var MowjowFunds = artifacts.require("./MowjowFunds.sol"); 
-var MowjowCrowdsale = artifacts.require('MowjowCrowdsale')
+var MowjowCrowdsale = artifacts.require('MowjowCrowdsale');
  
 
 module.exports =  async function (deployer, network, accounts) { 
 
-   const finalizableMowjow = FinalizableMowjow.deployed();   
-   const earlyContribStrategy = EarlyContribStrategy.deployed() ;
-   const preIcoStrategy = PreIcoStrategy.deployed();
-   const trancheStrategy = TrancheStrategy.deployed();
-   deployer.deploy(MowjowCrowdsale, 1510844468, 1513429708, 15000, accounts[1], 15000, earlyContribStrategy.address, preIcoStrategy.address, trancheStrategy.address, finalizableMowjow.address, testData.gasValue);
+   const finalizableMowjow = await FinalizableMowjow.deployed();
+   const earlyContribStrategy = await EarlyContribStrategy.deployed();
+   const preIcoStrategy = await PreIcoStrategy.deployed();
+   const trancheStrategy = await TrancheStrategy.deployed();
+   let startDate = new Date('12-15-2017').getTime();
+   let endDate = new Date('12-30-2017').getTime();
+    // 1510844468, 1513429708
+   deployer.deploy(MowjowCrowdsale, startDate, endDate, 15000, accounts[1], 15000,
+       earlyContribStrategy.address, preIcoStrategy.address, trancheStrategy.address,
+       finalizableMowjow.address, { "gas": 4700000 });
   // deployer.link(MowjowCrowdsale, MultiSigMowjow);
   // deployer.deploy(MultiSigMowjow,ownersMultisig, requiredConfirmations, testData.gasValue)
 };
--- migrations/3_multisig.js
+++ migrations/3_multisig.js
@@ -4,7 +4,7 @@ var MultiSigMowjow = artifacts.require('MultiSigMowjow')
 
 
 module.exports = async function (deployer, network, accounts) {
-    const ownersMultisig = [accounts[1], accounts[2], accounts[3], accounts[4], accounts[5], accounts[6]]
+    const ownersMultisig = [accounts[1], accounts[2], accounts[3], accounts[4], accounts[5], accounts[6]];
     const confirmationsTwoThirds = ownersMultisig.length * 2 / 3;
     const requiredConfirmations = Math.floor(confirmationsTwoThirds);
     deployer.deploy(MultiSigMowjow, ownersMultisig, requiredConfirmations, testData.gasValue)
